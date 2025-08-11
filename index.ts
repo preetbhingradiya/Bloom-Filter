fbb01bd58fe9a16420159aee87c71e6d178b076b
preetbhingradiya <preetbhingradiya6@example.com>
2025-08-11 22:44:56 +0530

    chnages

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ for (let index = 0; index < 10; index++) {
 
 // Add for NOT existing postive rate
 for (let index = 0; index < 10; index++) {
-  const id = uuid4().toString();
+  const id = uuid4().toString(); 
   nonexisting.add(id);
   allKeys.push(id);
 }
